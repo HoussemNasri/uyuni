6b40b5bc71024b12db74cac9f5c46da846aa49dd
HoussemNasri <houssemnasri2001@gmail.com>
2023-08-27 19:17:52 +0100

    Fix some checkstyle warnings

--- java/code/src/com/suse/manager/webui/controllers/CVEAuditController.java
+++ java/code/src/com/suse/manager/webui/controllers/CVEAuditController.java
@@ -19,6 +19,7 @@ import static com.suse.manager.webui.utils.SparkApplicationHelper.json;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.withCsrfToken;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.withUser;
 import static com.suse.manager.webui.utils.SparkApplicationHelper.withUserPreferences;
+
 import static spark.Spark.get;
 import static spark.Spark.post;
 
@@ -34,7 +35,6 @@ import com.redhat.rhn.manager.audit.CVEAuditSystem;
 import com.redhat.rhn.manager.audit.PatchStatus;
 import com.redhat.rhn.manager.audit.UnknownCVEIdentifierException;
 import com.redhat.rhn.manager.rhnset.RhnSetDecl;
-
 import com.suse.manager.webui.utils.gson.ResultJson;
 
 import org.apache.logging.log4j.LogManager;
