136d71007b6ffeb9214b425fa1952adffbf6ec47
HoussemNasri <houssemnasri2001@gmail.com>
2023-09-01 13:27:31 +0100

    Fix more checkstyle warnings

--- java/code/src/com/suse/manager/webui/controllers/CVEAuditController.java
+++ java/code/src/com/suse/manager/webui/controllers/CVEAuditController.java
@@ -48,6 +48,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
